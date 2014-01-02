9db9b7c45c4d4ce8f4574453875c70a8551578f0
Todd Cannon <cannonwt@guc.com>
2014-01-02 10:37:51 -0500

    Added project files

---
+++ testSpec/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  testSpec
+//
+//  Created by Todd Cannon on 1/2/14.
+//  Copyright (c) 2014 Greenville Utilities Commission. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ testSpec/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  testSpec
+//
+//  Created by Todd Cannon on 1/2/14.
+//  Copyright (c) 2014 Greenville Utilities Commission. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
